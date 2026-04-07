34ddcbb9fd5e61ee12d9d9404ee4d6584b3f76dc
AbdulRhman Ibnouf <administration@ibnouf.me>
2026-04-08 01:53:13 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 // ==================== IMPORTS ====================
-import { SUPABASE_URL, SUPABASE_ANON_KEY } from './config.js';
-
 // ==================== SUPABASE CLIENT ====================
 import { SUPABASE_URL, SUPABASE_ANON_KEY } from './config.js';
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
